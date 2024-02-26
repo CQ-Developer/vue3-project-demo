a43e8136af16859526f052867fd2d2530d9825c0
Chen <chenq_developer@126.com>
2024-02-26 15:46:40 +0800

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,47 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/vue3-essential"
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-essential'
     ],
-    "overrides": [
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
+            files: [
+                '.eslintrc.{js,cjs}'
             ],
-            "parserOptions": {
-                "sourceType": "script"
+            parserOptions: {
+                sourceType: 'script'
             }
         }
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 'latest',
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
     },
-    "plugins": [
-        "@typescript-eslint",
-        "vue"
+    plugins: [
+        '@typescript-eslint',
+        'vue'
     ],
-    "rules": {
+    rules: {
+        'no-var': 'error',
+        'no-unused-vars': 'off',
+        'no-unexpected-multiline': 'error',
+        'no-useless-escape': 'warn',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/prefer-ts-expect-error': 'error',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-namespace': 'off',
+        'vue/multi-word-component-names': 'off',
+        'vue/no-mutating-props': 'warn',
+        'vue/attribute-hyphenation': 'off'
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 ```sh
 npm init @eslint/config
 ```
+
+2. 安装插件(可选)
+
+```sh
+npm install -D @babel/eslint-parser
+```
+
+3. rules
+
+- [eslint](https://eslint.org/docs/latest/rules)
+
+- [vue](https://eslint.vuejs.org/rules/)
+
+- [ts](https://typescript-eslint.io/rules/)
